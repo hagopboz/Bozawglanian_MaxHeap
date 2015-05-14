c2ec7817337ac83192a9d40b73db0edb88da0804
Hagop <Hagop@134.71.203.179>
2015-05-14 14:26:26 -0700

    Part 2

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,28 +1,42 @@
+import java.util.Scanner;
 
 public class MaxHeap {
 
-    public static int[] Arr = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+    public static int[] Arr;
     public static int counter = 0;
+    static Scanner kb = new Scanner(System.in);
 
     public static void main(String[] args) {
+    	
+    	int choice = 0;
+    	int user;
+    	
         int kk;
-        for (kk = 0; kk <= Arr.length - 1; kk++) {
+        for (kk = Arr.length -1; kk >= 0; kk--) {
             heapM(Arr, kk);
-        }
+            }
 
         for (int krk = 0; krk < Arr.length; krk++) {
             System.out.println(Arr[krk]);
         }
-
-
-
+    	
+    	while (choice < 2) {
+        	System.out.println("===========================================================================");
+        	System.out.println("/n/n/n");
+        	System.out.println("Please select how to test the program:");
+        	System.out.println("(1) 20 sets of 100 randomly generated integers");
+        	System.out.println("(2) Fixed integer values 1-100");
+        	System.out.println("Enter choice:");
+        	user = kb.nextInt();
+    	}
+    	
     }
 
     public static void heapM(int[] Arr, int i) {
 
         int largest;
-        int left = i * 2;
-        int right = i * 2 + 1;
+        int left = 2*i+1;
+        int right = 2*i + 2;
         if (((left < Arr.length) && (Arr[left] > Arr[i]))) {
             largest = left;
         } else {
@@ -34,8 +48,6 @@ public class MaxHeap {
         }
         if (largest != i) {
             swap(i, largest);
-
-
             heapM(Arr, largest);
         }
     }
